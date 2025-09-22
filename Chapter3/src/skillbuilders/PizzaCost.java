db722fd206ce4c687f57bb1bf8d1c212007cb443
1100058736 <1100058736@L815WD220017.edu.cbe.ab.ca>
2025-09-22 11:18:48 -0600

    11:17AM

--- Chapter3/src/skillbuilders/PizzaCost.java
+++ Chapter3/src/skillbuilders/PizzaCost.java
@@ -1,30 +1,33 @@
-package skillbuilders;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class PizzaCost {
+    public class PizzaCost {
 
-	public static void main(String[] args) 
-	{
+    	public static void main(String[] args) 
+    	{
+    		//Get digits from user
+    	    System.out.print("Enter two digit numbers: ");
+    	    
+    	 try (// Create a Scanner object to read input from the console
+    		Scanner input = new Scanner(System.in)) {
+    			int number = input.nextInt();
+    			
+    			int tens, ones;
+    			
+    			tens = number / 10;
+    			ones = number % 10;
+    			
+    			System.out.println("Tens = "+ tens);
+    		}
+    	    
+    	    String Ones = null;
+    		System.out.print("Ones = "+ Ones);
 
-		 Scanner scanner = new Scanner(System.in);
-		 
-		 // Prompt user for pizza diameter
-	        System.out.print("Enter the diameter of the pizza in inches: ");
+    	}
 
-	        double diameter = scanner.nextDouble();
-	        double laborCost = 0.75;
-	        double rentCost = 1.00;
-	        double materialsCost = 0.05 * diameter * diameter;
-	        
-	        // Total cost
-	        double totalCost = laborCost + rentCost + materialsCost;
-	        
-	     // Display the result
-	        System.out.printf("The cost of making the pizza is: $%.2f%n ", totalCost);
-	        
-	        // Close the scanner
-	        scanner.close();
-	}
+    }
 
-}
+
+
+
+	
