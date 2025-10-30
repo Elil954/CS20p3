d05e75ce93b573c715f6a24e7129ae13f484a39f
1100058736 <1100058736@L815WD220017.edu.cbe.ab.ca>
2025-10-30 09:36:35 -0600

    9:36am

---
+++ Chapter5/src/skillbuilders/Evens.java
@@ -0,0 +1,31 @@
+package skillbuilders;
+
+public class Evens {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Even numbers between 1 and 30 (inclusive):");
+   
+		System.out.println("\n");
+		
+		
+
+        for (int i = 2; i <= 30; i += 2) {
+            
+            // Display the current even number
+            System.out.print(i + " ");
+        }
+
+        System.out.println(); // Print a final newline for clean formatting
+        System.out.println("----------------------------------------");
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
